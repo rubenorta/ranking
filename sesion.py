bacc8a5920d7b456873459b755682143020fc822
Ruben Orta <ruben.orta@gmail.com>
2013-07-10 00:17:51 +0200

    Adding issues list

--- sesion.py
+++ sesion.py
@@ -1,5 +1,7 @@
 import json
 from collections import defaultdict 
+from collections import Counter
+
 def get_counts(sequence):
     counts = defaultdict(int)
     for x in sequence:
@@ -10,10 +12,26 @@ def top_counts(count_dict, n=5):
     value_key_pairs = [(count, name) for name, count in count_dict.items()]
     value_key_pairs.sort()
     return value_key_pairs[-n:]
+    
+def open_issues(count_dict):
+    items = []
+    #for item in count_dict:   
+    #    if 'status' in item and item['status']['name'] == 'Nueva':
+    #        print item['status']
+    #        if item['status'] == 'Nueva':
+    #            print item['status']
+    #            items = (item['id'],item['status']['name'])
+    #return items 
+    
+    items = [(item['id'], item['project']['name'],item['subject']) 
+            for item in count_dict if 'status' in item and item['status']['name'] == 'Nueva']
+    return items
         
 path = "/home/ruben/Development/datapy/issues.json"
 issues = [json.loads(line) for line in open(path)]
 records = issues[0]['issues']
 ninjas = [rec['assigned_to']['name'] for rec in records if 'assigned_to' in rec]
-counts = get_counts(ninjas)
-
+status = [rec['status']['name'] for rec in records if 'status' in rec]
+#counts = get_counts(ninjas)
+counts = Counter(ninjas)
+o_issues = open_issues(records)
